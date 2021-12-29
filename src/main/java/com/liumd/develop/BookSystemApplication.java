e8ddb8bb2e4660ec07e35021d3f2edb913339869
liumuda <1161859914@qq.com>
2021-12-29 14:18:08 +0800

    book-system上传

---
+++ src/main/java/com/liumd/develop/BookSystemApplication.java
@@ -0,0 +1,13 @@
+package com.liumd.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSystemApplication.class, args);
+    }
+
+}
